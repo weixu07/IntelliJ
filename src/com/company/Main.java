88787048a0f6daed6cc22fbc356b2b512cd43081
Weixu Chen <wchen607@gatech.edu>
2021-01-19 22:17:19 -0500

    Initial add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello World");
         System.out.println("Hello " + args[0]);
+        System.out.println("Hello " + args[1]);
     }
 }
